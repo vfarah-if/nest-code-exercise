af95bd52e972a0bbe59068c12d783fc69f974c3c
Vincent Farah <vincent.farah@newlook.com>
2022-12-26 17:27:43 +0000

    Add dbcontext for initiating db context

---
+++ mock-petstore/src/db/db_context.ts
@@ -0,0 +1,57 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose, { connect, disconnect, Mongoose } from 'mongoose'
+import { config } from '../config'
+
+const dbContext = async (): Promise<Mongoose> => {
+  /*
+   *  NOTE: If this is failing, you have not setup the .env
+   *   file in the root of the project - see readme for more
+   */
+  const { dbUrl, dbName, dbUser, dbPassword, dbPort, isInMemoryDatabase } =
+    config
+  console.assert(dbUrl)
+  console.assert(dbName)
+  console.assert(dbPort)
+  let uri = dbUrl
+  let mongoDb: MongoMemoryServer = undefined
+  if (isInMemoryDatabase) {
+    console.debug(
+      `Preparing inMemoryDatabase ${dbName} on port ${dbPort} with user '${dbUser}'
+		 password '${dbPassword}'`,
+    )
+    const hasAuthDetails = dbUser || dbPassword ? true : false
+    mongoDb = await MongoMemoryServer.create({
+      auth: {
+        customRootName: dbUser,
+        customRootPwd: dbPassword,
+      },
+      instance: {
+        dbName,
+        port: dbPort,
+        auth: hasAuthDetails,
+      },
+    })
+  }
+
+  console.debug(
+    `Connecting to Mongo database ${dbName} with user '${
+      isInMemoryDatabase ? 'None' : dbUser
+    }' on URL '${isInMemoryDatabase ? mongoDb.getUri(dbName) : uri}'`,
+  )
+
+  const options = {
+    dbName: dbName,
+    user: dbUser,
+    pass: dbPassword,
+    autoIndex: true,
+    autoCreate: true,
+  }
+  mongoose.set('strictQuery', false)
+  return await connect(uri, options)
+}
+
+const closeDbContext = async (): Promise<void> => {
+  return await disconnect()
+}
+
+export { dbContext, closeDbContext }
