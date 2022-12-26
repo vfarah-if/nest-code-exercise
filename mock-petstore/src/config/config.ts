ac6c7fdb350c5583c98966098b26bf6a404dd5ee
Vincent Farah <vincent.farah@newlook.com>
2022-12-26 15:20:22 +0000

    Create config

---
+++ mock-petstore/src/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+  port: parseInt(process.env.PORT || '4444', 10),
+  isDev: process.env.NODE_ENV === 'development',
+  isProd: process.env.NODE_ENV === 'production',
+  isTest: process.env.NODE_ENV === 'test',
+  isInMemoryDatabase:
+    process.env.DATABASE_IN_MEMORY?.toLowerCase() === 'true' || true,
+  dbPort: parseInt(process.env.DB_PORT || '27017', 10),
+  dbUrl: process.env.DATABASE_URL,
+  dbName: process.env.DATABASE_DBNAME || 'petstore',
+  dbUser: process.env.DATABASE_USER,
+  dbPassword: process.env.DATABASE_PASS,
+}
+console.log(config)
+export { config }
