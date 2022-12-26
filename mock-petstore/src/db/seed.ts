9a62a6f2f1adada811c75d116dca91cc659f74c9
Vincent Farah <vincent.farah@newlook.com>
2022-12-26 17:26:57 +0000

    Add seeding method

---
+++ mock-petstore/src/db/seed.ts
@@ -0,0 +1,17 @@
+export async function seedDb(): Promise<void> {
+  // const mockEndpoint = await prismaService.mockendpoint.create({
+  //   data: {
+  //     url: 'v1/customers',
+  //     method: 'GET',
+  //     jsonResponse: JSON.stringify({
+  //       email: 'jane.doe@newlook.com',
+  //       firstName: 'Jane',
+  //       lastName: 'Doe',
+  //       mobileNumber: '+44 797396 7029',
+  //       dateOfBirth: '1974-11-04',
+  //       gender: 'Male',
+  //     }),
+  //   },
+  // })
+  // console.log(mockEndpoint)
+}
