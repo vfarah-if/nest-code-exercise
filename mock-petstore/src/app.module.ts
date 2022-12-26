bf22db1fdef1b1759fa06a3943e42849eea78183
Vincent Farah <vincent.farah@newlook.com>
2022-12-26 17:29:35 +0000

    Remove prisma as it does not work with mongodb-memory-server

--- mock-petstore/src/app.module.ts
+++ mock-petstore/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CustomersModule } from './customers/customers.module'
-import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    CustomersModule,
-    PrismaModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CustomersModule],
 })
 export class AppModule {}
