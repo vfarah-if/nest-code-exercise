ffd049719c160da99d3d6f90bf60b6a68067d4f4
Vincent Farah <vincent.farah@newlook.com>
2023-01-04 22:56:32 +0000

    Remove open-api-mocker as a bad idea

--- mock-vendors/src/main.ts
+++ mock-vendors/src/main.ts
@@ -1,5 +1,4 @@
 require('dotenv').config()
-import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { config } from './config'
@@ -7,52 +6,28 @@ import { closeDbContext, dbContext } from './db/db_context'
 import { seedDb } from './db/seed'
 const { port } = config
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  })
-  dbContext()
-    .then(() => {
-      console.debug('Connection to Mongo open')
-      // seed test data
-      seedDb().then((value) => {
-        console.debug('Finished seeding data', value)
-      })
-    })
-    .catch((error) => {
-      const warningMessage = `Unable to connect to the local Mongo 
-      (Make sure configured inside the .env file correctly)`
-      console.warn(warningMessage, error)
-    })
-  // Uncomment if you want to run Mocker framework
-  // await runOpenApiMocker()
-  // Comment if you want to run Mocker framework
-  await app.listen(port)
-
-  async function runOpenApiMocker() {
-    const options = {
-      port: port,
-      schema: './assets/customer-api.json',
-      watch: true,
-      servers: app,
-    }
-    const OpenApiMocker = require('open-api-mocker')
-    const mocker = new OpenApiMocker(options)
-    await mocker.validate()
-    await mocker.mock()
+  try {
+    await dbContext()
+    console.debug('Connection to Mongo open')
+    const seedResponse = await seedDb()
+    console.debug('Finished seeding data', seedResponse)
+  } catch (error) {
+    const warningMessage = `Unable to connect to the local Mongo 
+    (Make sure configured inside the .env file correctly)`
+    console.warn(warningMessage, error)
   }
+  await app.listen(port)
 }
 
 process.on('close', shutDown)
 process.on('SIGTERM', shutDown)
 process.on('SIGINT', shutDown)
-function shutDown() {
-  closeDbContext().then(() => {
-    console.log('Closed out remaining connections')
-    process.exit(0)
-  })
+async function shutDown(): Promise<void> {
+  await closeDbContext()
+  console.log('Closed out remaining connections')
+  process.exit(0)
 }
 
 bootstrap()
